1003a5b15fce97fcaa09fdc5fffd3b6c7cc9ca50
Eric Gilmore <egilmore@google.com>
2015-12-03 11:06:33 -0800

    Adding links back to Firebase cloud functions docs.wq
    Change-Id: I0b13a72e351a3aa413cbb163b448505f4e9e4a4a

--- gcf/functions/index.js
+++ gcf/functions/index.js
@@ -13,6 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * For more information on setting up and running this sample code, see
+ * https://developers.google.com/firebase/docs/cloud-functions/writing-functions
+ */
+
 'use strict';
 
 // [START imports]
