254c83c90223169672ba7e8044ce51e9c4eeb2ff
Arthur Thompson <arthurthompson@google.com>
2016-09-30 11:52:57 -0700

    Apply sample first comments
    - Use more "to the point" comments in included code
    - Remove SDK from sample since FCM will be included in core sdk
    
    Change-Id: Icd34ecbca77005827cf915b61a9610ea36b02268

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,8 +1,7 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available here.
-importScripts('https://www.gstatic.com/firebasejs/3.4.0/firebase-app.js');
-importScripts('/sdk/firebase-messaging-external_bin.js')
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase.js');
 
 // [START initialize_firebase_in_sw]
 // Initialize the Firebase app in the service worker by passing in the
