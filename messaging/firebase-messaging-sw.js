cbd8f984fdd54c2675b9b6c16b717090afd81504
Arthur Thompson <arthurthompson@google.com>
2016-10-03 16:48:11 -0700

    Add more detail to initialize include.
    Change-Id: I339e8e2a19e783dcf0b252e37b70b54910e55cc8

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,19 +1,20 @@
+// [START initialize_firebase_in_sw]
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
-// [START initialize_firebase_in_sw]
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
   'messagingSenderId': 'YOUR-SENDER-ID'
 });
-// [END initialize_firebase_in_sw]
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
 
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
