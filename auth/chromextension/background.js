48f0b88a1f4efee5d62e2738e2082ad20f4341af
Nicolas Garnier <nivco@google.com>
2017-05-08 14:53:43 +0200

    Fixing double quotes in JS String
    Change-Id: I135726769040d8050faa67eacd319f9df02e38d8

--- auth/chromextension/background.js
+++ auth/chromextension/background.js
@@ -1,9 +1,9 @@
 // TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
 // Initialize Firebase
 var config = {
-  apiKey: "<YOUR_API_KEY>",
-  databaseURL: "<YOUR_DATABASE_URL>",
-  storageBucket: "<YOUR_STORAGE_BUCKET_NAME>"
+  apiKey: '<YOUR_API_KEY>',
+  databaseURL: '<YOUR_DATABASE_URL>',
+  storageBucket: '<YOUR_STORAGE_BUCKET_NAME>'
 };
 firebase.initializeApp(config);
 
