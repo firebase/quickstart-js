6da9477ca926b8bf5de4ca971ceb88d4eae6d816
Maye Edwin <mayedwine1@gmail.com>
2019-08-10 17:52:17 +0300

    fix => error  'query' is constant  no-const-assign

--- firestore/scripts/FriendlyEats.Data.js
+++ firestore/scripts/FriendlyEats.Data.js
@@ -50,19 +50,19 @@ FriendlyEats.prototype.getFilteredRestaurants = (filters, render) => {
   const query = firebase.firestore().collection('restaurants');
 
   if (filters.category !== 'Any') {
-    query = query.where('category', '==', filters.category);
+    const query = query.where('category', '==', filters.category);
   }
 
   if (filters.city !== 'Any') {
-    query = query.where('city', '==', filters.city);
+    const query = query.where('city', '==', filters.city);
   }
 
   if (filters.price !== 'Any') {
-    query = query.where('price', '==', filters.price.length);
+    const query = query.where('price', '==', filters.price.length);
   }
 
   if (filters.sort === 'Rating') {
-    query = query.orderBy('avgRating', 'desc');
+    const query = query.orderBy('avgRating', 'desc');
   } else if (filters.sort === 'Reviews') {
     query = query.orderBy('numRatings', 'desc');
   }
