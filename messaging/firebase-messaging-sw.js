15a81b3581f24845e3a158924efdbbc50a6db86c
kai <zwu52@uw.edu>
2020-10-14 16:24:49 -0700

    Update Quick Start for new API

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,9 +1,9 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('/__/firebase/7.12.0/firebase-app.js');
-importScripts('/__/firebase/7.12.0/firebase-messaging.js');
-importScripts('/__/firebase/init.js');
+importScripts("/__/firebase/7.23.0/firebase-app.js");
+importScripts("/__/firebase/7.23.0/firebase-messaging.js");
+importScripts("/__/firebase/init.js");
 
 const messaging = firebase.messaging();
 
@@ -38,21 +38,23 @@ const messaging = firebase.messaging();
  // [END initialize_firebase_in_sw]
  **/
 
-
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
 // implement this optional method.
-// [START background_handler]
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+// [START onBackgroundMessage]
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
   // Customize notification here
-  const notificationTitle = 'Background Message Title';
+  const notificationTitle = "Background Message Title";
   const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
+    body: "Background Message body.",
+    icon: "/firebase-logo.png",
   };
 
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
-// [END background_handler]
+// [END onBackgroundMessage]
