901fed72ec950dc8d71fbf988d3b8e835cd37a9c
Sam <samstern@google.com>
2021-01-15 12:09:14 +0000

    Remove remaining snippet tags

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -11,7 +11,6 @@ const messaging = firebase.messaging();
  * Here is is the code snippet to initialize Firebase Messaging in the Service
  * Worker when your app is not hosted on Firebase Hosting.
 
- // [START initialize_firebase_in_sw]
  // Give the service worker access to Firebase Messaging.
  // Note that you can only use Firebase Messaging here. Other Firebase libraries
  // are not available in the service worker.
@@ -35,14 +34,12 @@ const messaging = firebase.messaging();
  // Retrieve an instance of Firebase Messaging so that it can handle background
  // messages.
  const messaging = firebase.messaging();
- // [END initialize_firebase_in_sw]
  **/
 
 
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
 // implement this optional method.
-// [START on_background_message]
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   // Customize notification here
@@ -55,4 +52,3 @@ messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(notificationTitle,
     notificationOptions);
 });
-// [END on_background_message]
