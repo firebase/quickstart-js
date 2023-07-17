7146944de558b83d7d67d69c6df8854bdba52cad
Alphonso Bradham <bradhamalphonso@gmail.com>
2023-07-17 16:24:46 +0000

    Updated to work with demo-project cli premix

---
+++ firestore/angular-rewrite/src/environments/environment.ts
@@ -0,0 +1,10 @@
+/**  If connecting to a live Firebase project (one that you set up in the 
+ * Firebase console) put your config vars here. Otherwise, leave blank to 
+ * enable connection to demo project.
+*/
+export const environment = {
+    firebase: {
+        projectId: 'demo-friendly-eats',
+        apiKey: 'random'
+    }
+}
