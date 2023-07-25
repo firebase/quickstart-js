ba8b006fb853e3d13674d08eed47c1c7ac39f2e1
Alphonso Bradham <bradhamalphonso@gmail.com>
2023-07-25 19:26:17 +0000

    Moved up to parent dir

---
+++ firestore/src/app/filter-dialog/dialogdata.ts
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+// TODO(abradham): Move to separate file to make accessible for emulators
+export interface DialogData {
+    sortBy: 'Rating' | 'Reviews';
+    category: string;
+    city: string;
+    price: number;
+}
+
+export const DEFAULT_SORT_DATA: DialogData = {
+    sortBy: 'Rating',
+    category: 'Any',
+    city: 'Any',
+    price: -1
+}
+
+export const filterCities = [
+    'Any',
+    'Albuquerque',
+    'Arlington',
+    'Atlanta',
+    'Austin',
+    'Baltimore',
+    'Boston',
+    'Charlotte',
+    'Chicago',
+    'Cleveland',
+    'Colorado Springs',
+    'Columbus',
+    'Dallas',
+    'Denver',
+    'Detroit',
+    'El Paso',
+    'Fort Worth',
+    'Fresno',
+    'Houston',
+    'Indianapolis',
+    'Jacksonville',
+    'Kansas City',
+    'Las Vegas',
+    'Long Island',
+    'Los Angeles',
+    'Louisville',
+    'Memphis',
+    'Mesa',
+    'Miami',
+    'Milwaukee',
+    'Nashville',
+    'New York',
+    'Oakland',
+    'Oklahoma',
+    'Omaha',
+    'Philadelphia',
+    'Phoenix',
+    'Portland',
+    'Raleigh',
+    'Sacramento',
+    'San Antonio',
+    'San Diego',
+    'San Francisco',
+    'San Jose',
+    'Tucson',
+    'Tulsa',
+    'Virginia Beach',
+    'Washington'
+]
+
+export const filterCategories = [
+    'Any',
+    'Brunch',
+    'Burgers',
+    'Coffee',
+    'Deli',
+    'Dim Sum',
+    'Indian',
+    'Italian',
+    'Mediterranean',
+    'Mexican',
+    'Pizza',
+    'Ramen',
+    'Sushi'
+]
---
+++ firestore/src/app/review-list/review-list.component.ts
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Component, OnInit, Input, inject } from '@angular/core';
+import { Rating } from '../../../types/ratings';
+import { Observable } from 'rxjs';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+
+@Component({
+  selector: 'app-review-list',
+  templateUrl: './review-list.component.html',
+  styleUrls: ['./review-list.component.css']
+})
+export class ReviewListComponent implements OnInit {
+  @Input() restaurantID: string | null = null;
+  private firestore: Firestore = inject(Firestore);
+  reviews: Observable<Rating[]> = new Observable();
+
+  ngOnInit(): void {
+    const ratingsRef = collection(this.firestore, `/restaurants/${this.restaurantID}/ratings`);
+    this.reviews = collectionData(ratingsRef, { idField: 'id' }) as Observable<Rating[]>;
+  }
+}
