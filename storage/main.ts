da7c390fc96f22fd8b275c8d0ee9f4752184948f
Salakar <mike@invertase.io>
2023-08-30 14:25:37 +0530

    fix: nits

--- storage/main.ts
+++ storage/main.ts
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, connectAuthEmulator, signInAnonymously } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth, connectAuthEmulator, signInAnonymously } from 'firebase/auth';
 import {
   connectStorageEmulator,
   getStorage,
   ref,
   uploadBytes,
   getDownloadURL,
-} from "firebase/storage";
-import { firebaseConfig } from "../config";
+} from 'firebase/storage';
+import { firebaseConfig } from '../config';
 
-const fileInput = document.getElementById("file") as HTMLInputElement;
-const linkBox = document.getElementById("linkbox") as HTMLDivElement;
+const fileInput = document.getElementById('file') as HTMLInputElement;
+const linkBox = document.getElementById('linkbox') as HTMLDivElement;
 
 /**
  * When running on Firebase Hosting, no options need to be passed.
@@ -22,9 +22,9 @@ const auth = getAuth();
 const storage = getStorage();
 
 // Locally, we use the firebase emulators.
-if (window.location.hostname === "localhost") {
-  connectAuthEmulator(auth, "http://localhost:9099");
-  connectStorageEmulator(storage, "localhost", 9199);
+if (window.location.hostname === 'localhost') {
+  connectAuthEmulator(auth, 'http://127.0.0.1:9099');
+  connectStorageEmulator(storage, '127.0.0.1', 9199);
 }
 
 const storageRef = ref(storage);
@@ -42,38 +42,38 @@ function handleFileSelect(e: Event) {
   const file = files[0];
 
   // Push to child path.
-  uploadBytes(ref(storageRef, "images/" + file.name), file)
+  uploadBytes(ref(storageRef, 'images/' + file.name), file)
     .then(function (snapshot) {
-      console.log("Uploaded", snapshot.metadata.size, "bytes.");
-      console.log("File metadata:", snapshot.metadata);
+      console.log('Uploaded', snapshot.metadata.size, 'bytes.');
+      console.log('File metadata:', snapshot.metadata);
       // Let's get a download URL for the file.
       getDownloadURL(snapshot.ref).then(function (url) {
-        console.log("File available at", url);
+        console.log('File available at', url);
         linkBox.innerHTML = '<a href="' + url + '">Click For File</a>';
       });
     })
     .catch(function (error) {
-      console.error("Upload failed:", error);
+      console.error('Upload failed:', error);
     });
 }
 
-fileInput.addEventListener("change", handleFileSelect, false);
+fileInput.addEventListener('change', handleFileSelect, false);
 fileInput.disabled = true;
 
 auth.onAuthStateChanged(function (user) {
   if (user) {
-    console.log("Anonymous user signed-in.", user);
+    console.log('Anonymous user signed-in.', user);
     fileInput.disabled = false;
   } else {
     console.log(
-      "There was no anonymous session. Creating a new anonymous user.",
+      'There was no anonymous session. Creating a new anonymous user.'
     );
     // Sign the user in anonymously since accessing Storage requires the user to be authorized.
     signInAnonymously(auth).catch(function (error) {
-      if (error.code === "auth/operation-not-allowed") {
+      if (error.code === 'auth/operation-not-allowed') {
         window.alert(
-          "Anonymous Sign-in failed. Please make sure that you have enabled anonymous " +
-            "sign-in on your Firebase project.",
+          'Anonymous Sign-in failed. Please make sure that you have enabled anonymous ' +
+            'sign-in on your Firebase project.'
         );
       }
     });
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: "/",
+  base: '/',
   build: {
     rollupOptions: {
       input: {
-        storage: "./storage/index.html",
+        storage: './storage/index.html',
       },
     },
   },
