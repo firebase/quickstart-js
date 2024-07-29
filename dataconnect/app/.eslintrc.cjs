3a75679a684c76cb030768159add73c0db7f6b92
Cynthia Wang <hyacccint@gmail.com>
2024-07-29 19:39:37 -0400

    feat: update eslint

--- dataconnect/app/.eslintrc.cjs
+++ dataconnect/app/.eslintrc.cjs
@@ -4,15 +4,16 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
   },
 }
