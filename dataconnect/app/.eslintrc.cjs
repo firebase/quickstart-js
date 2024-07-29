b15d8f4d611da95439eba7a0a8971aad8e09e167
Cynthia Wang <hyacccint@gmail.com>
2024-07-29 18:42:32 -0400

    fix: plugins

--- dataconnect/app/.eslintrc.cjs
+++ dataconnect/app/.eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
