35299367dc9c7836f21a69c553d822fd656c5518
deast <deast@google.com>
2024-10-01 21:29:44 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -10,9 +10,9 @@
     cp -a ${./app}/* "$out"/app/
     cp -a ${./dataconnect}/* "$out"/dataconnect/
     cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
+    chmod -R u+w "$out"
     rm "$out"/app/src/lib/firebase.tsx
     rm "$out"/app/vite.config.ts
-    chmod -R u+w "$out"
     mv ${./app/src/lib/firebase.idx.tsx} "$out"/app/src/lib/firebase.tsx
     mv ${./app/vite.config.idx.ts} "$out"/app/vite.config.ts
   '';
