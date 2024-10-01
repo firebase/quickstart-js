3a93b4005c00fab49c7fdfd54e74926479d04239
deast <deast@google.com>
2024-10-01 16:16:03 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -7,8 +7,8 @@
     mkdir "$out"/dataconnect
     mkdir "$out"/.vscode
     cp ${.idx/dev.nix} "$out"/.idx/dev.nix
-    cp -a ./app "$out"/app
-    cp -a ./dataconnect "$out"/dataconnect
+    cp -a ${./app} "$out"/app
+    cp -a ${./dataconnect} "$out"/dataconnect
     cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
     chmod -R u+w "$out"
   '';
