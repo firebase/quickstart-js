95cbd51125c88562221f6d650b2415f645fa0dc5
deast <deast@google.com>
2024-10-01 15:28:10 +0000

    idx template

---
+++ dataconnect/idx-template.nix
@@ -0,0 +1,15 @@
+{pkgs, ... }: {
+  packages = [];
+  bootstrap = ''
+    mkdir "$out"
+    mkdir "$out"/.idx
+    mkdir "$out"/app
+    mkdir "$out"/dataconnect
+    mkdir "$out"/.vscode
+    cp ${.idx/dev.nix} "$out"/.idx/dev.nix
+    cp -a ./app "$out"/app
+    cp -a ./dataconnect "$out"/dataconnect
+    cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
+    chmod -R u+w "$out"
+  '';
+}
