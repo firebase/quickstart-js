ba4a4c02ab108bc37ae540ed600d6db9210c2fdb
deast <deast@google.com>
2024-10-02 16:11:42 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -17,29 +17,16 @@
     mkdir "$out"/dataconnect
     mkdir "$out"/.vscode
 
-    # Create a temporary directory
-    tmpdir=$(mktemp -d) 
-    cp -a ${./app}/* "$tmpdir"/app/ 
-
-    # Rename the files within the temporary directory
-    mv "$tmpdir"/app/src/lib/firebase.tsx "$tmpdir"/app/src/lib/firebase.reg.tsx
-    mv "$tmpdir"/app/vite.config.ts "$tmpdir"/app/vite.config.reg.ts
-
     cp ${.idx/dev.nix} "$out"/.idx/dev.nix
-    cp -a "$tmpdir"/app/* "$out"/app/  # Copy from the temporary directory
+    cp -a ${./app}/* "$out"/app/
     cp -a ${./dataconnect}/* "$out"/dataconnect/
-
-    # Use the new file names (from the temporary directory)
-    cp "$tmpdir"/app/src/lib/firebase.reg.tsx "$out"/app/src/lib/firebase.tsx  
-    cp "$tmpdir"/app/vite.config.reg.ts "$out"/app/vite.config.ts 
 
     cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
     cp ${./firebase.json} "$out"/firebase.json
     cp ${./README.md} "$out"/README.md
     cp ${./.gitignore} "$out"/.gitignore
     chmod -R u+w "$out" 
-
-    # Clean up the temporary directory
-    rm -rf "$tmpdir"
+    mv "$out"/app/src/lib/firebase.idx.tsx "$out"/app/src/lib/firebase.tsx  
+    mv "$out"/app/vite.config.idx.ts "$out"/app/vite.config.ts 
   '';
 }
