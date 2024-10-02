76bea8e05355ee8801b75a3156c8cdc601895b27
deast <deast@google.com>
2024-10-02 14:06:27 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -7,8 +7,8 @@
     mkdir "$out"/dataconnect
     mkdir "$out"/.vscode
 
-    sed 's/firebase.idx.tsx/firebase.tsx/' app/src/lib/firebase.idx.tsx > app/src/lib/firebase.tsx
-    sed 's/vite.config.idx.ts/vite.config.ts/' app/vite.config.idx.ts > app/vite.config.ts
+    mv ${./app/src/lib/firebase.idx.tsx} ${./app/src/lib/firebase.tsx}
+    mv ${./app/vite.config.idx.ts} ${./app/vite.config.ts}
 
     cp ${.idx/dev.nix} "$out"/.idx/dev.nix
     cp -a ${./app}/* "$out"/app/
