c3dd7c01296ac5f69fc4c45c751a62d060858943
deast <deast@google.com>
2024-10-02 15:27:31 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -7,19 +7,12 @@
   --failure-report
 
 */
+
 {pkgs, ... }: {
   packages = [];
   bootstrap = ''
-    mkdir "$out" 
-    mkdir "$out"/.idx
-    mkdir "$out"/app
-    mkdir "$out"/dataconnect
-    mkdir "$out"/.vscode
-    cp ${.idx/dev.nix} "$out"/.idx/dev.nix
-    cp -a ${./app}/* "$out"/app/
-    cp -a ${./dataconnect}/* "$out"/dataconnect/
-    
-    cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
+    mkdir "$out"
+    cp -a --parents ${.}/* "$out"
     chmod -R u+w "$out" 
   '';
 }
