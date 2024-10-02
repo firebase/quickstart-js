03d4d0b35fb57fe587676947fb3b92032b770767
deast <deast@google.com>
2024-10-02 14:49:48 +0000

    .

--- dataconnect/idx-template.nix
+++ dataconnect/idx-template.nix
@@ -1,7 +1,7 @@
 {pkgs, ... }: {
   packages = [];
   bootstrap = ''
-    mkdir "$out"
+    mkdir "$out" 
     mkdir "$out"/.idx
     mkdir "$out"/app
     mkdir "$out"/dataconnect
@@ -9,9 +9,9 @@
     cp ${.idx/dev.nix} "$out"/.idx/dev.nix
     cp -a ${./app}/* "$out"/app/
     cp -a ${./dataconnect}/* "$out"/dataconnect/
-    cp ${./app/src/lib/firebase.idx.tsx} "$out/app/src/lib/firebase.tsx"
-    cp ${./app/vite.config.idx.ts} "$out/app/vite.config.ts"
+    cp ${./app/src/lib/firebase.idx.tsx} "$out"/app/src/lib/firebase.tsx"
+    cp ${./app/vite.config.idx.ts} "$out"/app/vite.config.ts"
     cp ${./.vscode/settings.json} "$out"/.vscode/settings.json
-    chmod -R u+w "$out"
+    chmod -R u+w "$out" 
   '';
 }
