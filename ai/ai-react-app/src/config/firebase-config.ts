30fa97b900dcb40aa7ba9fd83d810faba2489550
Daniel La Rocque <dlarocque@google.com>
2025-05-12 11:38:08 -0400

    Move app to 'ai-react-app' dir

---
+++ ai/ai-react-app/src/config/firebase-config.ts
@@ -0,0 +1,9 @@
+// TODO (developer): Replace with your Firebase project configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCI_e1wo0T-SUbjKJU0LzI1kGOYvEDTnSE",
+  authDomain: "vertexaiinfirebase-test.firebaseapp.com",
+  projectId: "vertexaiinfirebase-test",
+  storageBucket: "vertexaiinfirebase-test.firebasestorage.app",
+  messagingSenderId: "857620473716",
+  appId: "1:857620473716:web:8aeae18386c04b63bb6e21"
+};
