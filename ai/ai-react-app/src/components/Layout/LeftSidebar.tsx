84b75c540cf1abcf5c6e366d9108f6c5372773c4
Paul Beusterien <paulbeusterien@google.com>
2025-08-07 07:22:27 -0700

    Update ai/ai-react-app/src/components/Layout/LeftSidebar.tsx
    Co-authored-by: Daniel La Rocque <dlarocque@google.com>

--- ai/ai-react-app/src/components/Layout/LeftSidebar.tsx
+++ ai/ai-react-app/src/components/Layout/LeftSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { AppMode } from "../../App";
 import styles from "./LeftSidebar.module.css";
 import { BackendType, Content, ModelParams } from "firebase/ai";
